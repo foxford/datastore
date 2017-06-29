3555f341a4e8de80b64b492f672c84cc2a371c1f
Andrei Nesterov <ae.nesterov@gmail.com>
2017-06-29 16:36:48 +0300

    Update Cowboy to 2.0.0-pre.10

--- src/datastore_constraint.erl
+++ src/datastore_constraint.erl
@@ -7,7 +7,7 @@
 ]).
 
 %% Types
--type constraint() :: fun((iodata()) -> {true, any()} | true | false).
+-type constraint() :: fun((forward | reverse | format_error, iodata()) -> {ok, any()} | {error, any()} | iodata()).
 
 %% =============================================================================
 %% API
@@ -15,22 +15,37 @@
 
 -spec binary(non_neg_integer(), non_neg_integer()) -> constraint().
 binary(Min, Max) ->
-	fun(Val) ->
-		try
-			Size = iolist_size(Val),
-			true = Size =< Max,
-			true = Size >= Min,
-			true
-		catch _:_ -> false end
+	fun
+		(forward, Val) ->
+			try
+				Size = iolist_size(Val),
+				true = Size =< Max,
+				true = Size >= Min,
+				{ok, Val}
+			catch _:_ -> {error, {invalid_binary, Val}} end;
+		(reverse, Val) ->
+			{ok, Val};
+		(format_error, {invalid_binary, Val}) ->
+			<<"The value ", Val/binary,
+				"should be a binary whose length is (", (integer_to_binary(Min))/binary,
+				$,, (integer_to_binary(Max))/binary, ").">>
 	end.
 
 -spec int(non_neg_integer(), non_neg_integer()) -> constraint().
 int(Min, Max) ->
-	fun(Val) ->
-		try
-			Num = binary_to_integer(Val),
-			true = Num =< Max,
-			true = Num >= Min,
-			{true, Num}
-		catch _:_ -> false end
+	fun
+		(forward, Val) ->
+			try
+				Num = binary_to_integer(Val),
+				true = Num =< Max,
+				true = Num >= Min,
+				{ok, Num}
+			catch _:_ -> {error, {invalid_integer, Val}} end;
+		(reverse, Val) ->
+			try {ok, integer_to_binary(Val)}
+			catch _:_ -> {error, {invalid_integer, Val}} end;
+		(format_error, {invalid_integer, Val}) ->
+			<<"The value ", (integer_to_binary(Val))/binary,
+				"should be an integer whose range is (", (integer_to_binary(Min))/binary,
+				$,, (integer_to_binary(Max))/binary, ").">>
 	end.
